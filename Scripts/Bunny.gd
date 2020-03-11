f9074d6587e94cfa96450c63a4c634ea522e7e69
Elijah Atkins <elijahorama@gmail.com>
2020-03-11 15:31:15 -0600

    finished animation rigging

--- Scripts/Bunny.gd
+++ Scripts/Bunny.gd
@@ -3,19 +3,20 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 500
-const GRAVITY = 2
+const GRAVITY = 9.8
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 8
+const JUMP_SPEED = 500
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion*SPEED, UP)
 
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
-		motion.y -= JUMP_SPEED 
+		motion.y -= JUMP_SPEED
 	
 func move():
 	motion.x = Input.get_action_strength("right") - Input.get_action_strength("left")
@@ -25,3 +26,16 @@ func apply_gravity():
 		motion.y += GRAVITY
 	else:
 		motion.y = 0
+
+func animate():
+	$AnimatedSprite.flip_h 
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	elif motion.x < 0:
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
