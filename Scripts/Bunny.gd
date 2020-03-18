e3d87ac7acebd60700cad0b35b6c8bbff10b9b2c
Elijah Atkins <elijahorama@gmail.com>
2020-03-18 14:30:56 -0600

    adjusted charactor controls

--- Scripts/Bunny.gd
+++ Scripts/Bunny.gd
@@ -2,28 +2,28 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 500
+const SPEED = 1000
 const GRAVITY = 9.8
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 500
+const JUMP_SPEED = 2.5
 
 func _physics_process(delta):
-	apply_gravity()
-	jump()
-	move()
+	apply_gravity(delta)
+	jump(delta)
+	move(delta)
 	animate()
 	move_and_slide(motion*SPEED, UP)
 
-func jump():
+func jump(delta):
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 	
-func move():
+func move(delta):
 	motion.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 
-func apply_gravity():
+func apply_gravity(delta):
 	if !is_on_floor():
-		motion.y += GRAVITY
+		motion.y += GRAVITY*delta
 	else:
 		motion.y = 0
 
